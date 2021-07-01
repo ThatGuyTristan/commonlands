c86d3d8fc47d62c62d79c3a714eb778f5fb87066
bradleyaellis <bradleyaellis@gmail.com>
2021-06-30 20:50:30 -0500

    Refactor inventory

---
+++ src/assets/js/items.js
@@ -0,0 +1,7 @@
+const items = [
+  { name: "Potion", method: "usePotion", image: "" },
+  { name: "Throwing Knife", method: "useThrowingKnife", image: "" },
+  { name: "Smoke Bomb", method: "useSmokeBomb", image: "" },
+];
+
+export default items;
--- src/components/hero/inventory.vue
+++ src/components/hero/inventory.vue
@@ -1,26 +1,33 @@
 <template lang="pug">
   v-row
     v-col(cols="5")
-      div.mx-auto(v-for="(item, i) in items") {{ item }}
+      div.mx-auto(v-for="(item, i) in value")
+        v-btn(@click="log(item.method)") {{ item.name }}
 </template>
 
 <script>
+import itemLibrary from '@/assets/js/items.js';
+
 export default {
   data() {
     return {
-      items: [
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-        "Empty",
-      ],
+      items: itemLibrary,
+      value: [],
     };
   },
-  methods: {},
+  created() {
+    console.log(this.value);
+    this.value = this.value.concat(this.items);
+  },
+  watch: {
+    value(val){
+      if (val) console.log('here', val);
+    }
+  },
+  methods: {
+    log(foo){
+      console.log(foo);
+    },
+  },
 };
 </script>
