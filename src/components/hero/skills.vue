99af662629e03c96602288a86b875f80c06562de
bradleyaellis <bradleyaellis@gmail.com>
2021-07-08 10:32:37 -0500

    Rm dumb folder

--- src/components/hero/skills.vue
+++ src/components/hero/skills.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   v-row
     v-col
-      v-btn(:disabled="!item.used && !findCondition(item.condition)" :color="item.color" v-for="item in skills") {{ item.name }}
+      v-btn(:disabled="!findCondition(item.condition)" :color="item.color" v-for="item in skills") {{ item.name }}
 
 </template>
 
